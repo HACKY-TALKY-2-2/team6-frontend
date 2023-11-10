7d8800eef4c5da1414688531ad3b84814b02cf18
yuseong01 <dog2655@naver.com>
2023-11-11 02:12:00 +0900

    fix: App.js에서 map불러오기로 변경 App.js에서 MainContent에서 map불러오도록 변경

--- src/index.js
+++ src/index.js
@@ -4,29 +4,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-var mapOptions = {
-  center: new naver.maps.LatLng(37.501, 127.037),
-  zoom: 17,
-};
 
-var map = new naver.maps.Map("map", mapOptions);
-const gsTowerMarker = new naver.maps.Marker({
-  map: map,
-  position: new naver.maps.LatLng(37.50185, 127.0371),
-});
-const root = ReactDOM.createRoot(document.getElementById("header"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Header />
+    <App naver={naver} />
   </React.StrictMode>
 );
 
-const footer = ReactDOM.createRoot(document.getElementById("footer"));
-footer.render(<Footer/>)
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
