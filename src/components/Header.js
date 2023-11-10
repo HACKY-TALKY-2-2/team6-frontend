e86ab4759f78bac9d0c938054fd277b0c75b1c15
yuseong01 <dog2655@naver.com>
2023-11-11 01:27:28 +0900

    feat: 기본레이아웃 구성 Header, Footer 구성 및 지도 위아래로 추가

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const HeaderWrapper = styled.header`
+  font-size: 24px;
+  margin-bottom: 20px;
+`;
+
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      웹페이지 기본 레이아웃
+    </HeaderWrapper>
+  );
+};
+
+export default Header;
