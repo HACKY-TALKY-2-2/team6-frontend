184353b9f3d4a0e5b4fffde8a8f4aaae450c32d3
yuseong01 <dog2655@naver.com>
2023-11-11 06:37:37 +0900

    feat: 버스, 지하철 페이지 페이지 각각 따로 제작 후 App.js에서 라우팅

--- src/index.js
+++ src/index.js
@@ -4,18 +4,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import BusArrivalMessage from "./BusArrivalMessage";
-import { BezierProvider, LightFoundation } from "@channel.io/bezier-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App naver={naver} />
-    <div style={{ marginLeft: "20px", marginTop: "20px" }}>
-   <BezierProvider foundation={LightFoundation}>
-    <BusArrivalMessage />
-   </BezierProvider>
-  </div>
   </React.StrictMode>
 );
 
